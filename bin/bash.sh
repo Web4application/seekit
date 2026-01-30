ca67836ab7911e983e9599727e0c17a6e9610c27
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-30 23:38:24 +0100

    Update bash.sh

--- bin/bash.sh
+++ bin/bash.sh
@@ -1,2 +1,6 @@
 dotnet add package Algolia.Search --version 7.0.0
+dotnet new console -n AlgoliaExample
+cd AlgoliaExample
+dotnet add package Algolia.Search --version 7.0.0
+# .dll
 dotnet add package Algolia.Search --version 7.0.0
