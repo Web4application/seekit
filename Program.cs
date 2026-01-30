7615df10ecee36e40f6269cc455cffb1738307ee
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-30 23:39:19 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Algolia.Search.Clients;
+using Algolia.Search.Models.Search;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // 1. Initialize the Client
+        // Get these from your dashboard: 
